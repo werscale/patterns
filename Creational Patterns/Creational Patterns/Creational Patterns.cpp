b7c5e4318c79ea71ceab912570bf3b19bf5a4496
werscale <pavscale@gmail.com>
2026-02-27 19:08:58 +0500

    Добавил коментарии

--- Creational Patterns/Creational Patterns/Creational Patterns.cpp
+++ Creational Patterns/Creational Patterns/Creational Patterns.cpp
@@ -11,34 +11,104 @@ public:
     virtual void attack() = 0;
     virtual int getHealth() = 0;
 };
+
+/// \brief Класс с настройками для персонажа ВОИН
 class Warrior : public Character {
 public:
+    /**
+    * brief Фнункция для получения типа персонажа
+    * return string - строку, название персонажа
+    * remark Получаем только имя персонажа т.к. это учебный код
+    */
     string getType() override { return "Воин"; }
+    /**
+    * brief Фнункция для выполения действия персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Воин атакует мечом!" << endl; }
+    /**
+    * brief Фнункция для обозначения здоровья персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     int getHealth() override { return 100; }
 };
+/// \brief Класс с настройками для персонажа МАН
 class Mage : public Character {
 public:
+    /**
+    * brief Фнункция для получения типа персонажа
+    * return string - строку, название персонажа
+    * remark Получаем только имя персонажа т.к. это учебный код
+    */
     string getType() override { return "Маг"; }
+    /**
+    * brief Фнункция для выполения действия персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Маг кастует огненный шар!" << endl; }
+    /**
+    * brief Фнункция для обозначения здоровья персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     int getHealth() override { return 50; }
 };
+/// \brief Класс с настройками для персонажа ЛУЧНИК
 class Archer : public Character {
 public:
+    /**
+    * brief Фнункция для получения типа персонажа
+    * return string - строку, название персонажа
+    * remark Получаем только имя персонажа т.к. это учебный код
+    */
     string getType() override { return "Лучник"; }
+    /**
+    * brief Фнункция для выполения действия персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Лучик стреляет из лука!" << endl; }
+    /**
+    * brief Фнункция для обозначения здоровья персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     int getHealth() override { return 70; }
 };
+/// \brief Класс с настройками для персонажа РЫЦАРЬ
 class Knight : public Character {
 public:
+    /**
+    * brief Фнункция для получения типа персонажа
+    * return string - строку, название персонажа
+    * remark Получаем только имя персонажа т.к. это учебный код
+    */
     string getType() override { return "Рыцарь"; }
+    /**
+    * brief Фнункция для выполения действия персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Рыцарь атакует копьём!" << endl; }
+    /**
+    * brief Фнункция для обозначения здоровья персонажа
+    * note Не влияет ни на что. Это учебный код
+    * todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     int getHealth() override { return 120; }
 };
 
 class CharacterFactory {
 public:
     virtual Character* createCharacter() = 0;
+    /**
+    * brief Фнункция для создания персонажа и выполнения базовых его действий 
+    * note После создания объекта, выполняет действие персонажа и уничтожает объект
+    * warning Нельзя будет использовтаь созданных персонжаей в других частях кода
+    */
     void spawnAndAttack() {
         Character* chara = createCharacter();
         cout << "Создан " << chara->getType() << ", " << "со здоровьем: " << chara->getHealth() << " ";
@@ -49,6 +119,11 @@ public:
 
 class WarriorFactory : public CharacterFactory {
 public:
+    /**
+    * brief Фнункция для выделения памяти на класс персонажа 
+    * return Возвращает указатель на созданного персонажа
+    * 
+    */
     Character* createCharacter() override { return new Warrior(); }
 };
 class MageFactory : public CharacterFactory {
@@ -64,6 +139,8 @@ public:
     Character* createCharacter() override { return new Knight(); }
 };
 
+/// \brief Функция для запуска конвеера с персонажами
+/// \note Испольузется для учебного кода
 int main()
 {
     setlocale(LC_ALL, "ru");
