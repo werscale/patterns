37edb5198445f65510df8dc1b57c3e7616693b74
werscale <pavscale@gmail.com>
2026-02-27 19:44:43 +0500

    Добавил коменты

--- Behavioral Patterns/Behavioral Patterns/Behavioral Patterns.cpp
+++ Behavioral Patterns/Behavioral Patterns/Behavioral Patterns.cpp
@@ -1,60 +1,168 @@
 ﻿#include <iostream> 
 #include <Windows.h>
 #include <vector>
+
 using namespace std;
 
+/// \brief Класс системы компьютера
 class ComputerSystem
 {
 public:
-    void openFile(const string& file) { cout << "Открываем файл: " << file << endl; }
-    void shutdown() { cout << "Выключаем компьютер" << endl; }
-    void launchBrowser() { cout << "Запускаем браузер" << endl; }
+    /**
+    * brief Открытие файла
+    * note Метод выводит сообщение об открытии файла
+    */
+    void openFile(const string& file)
+    {
+        cout << "Открываем файл: " << file << endl;
+    }
+
+    /**
+    * brief Выключение компьютера
+    */
+    void shutdown()
+    {
+        cout << "Выключаем компьютер" << endl;
+    }
+
+    /**
+    * brief Запуск браузера
+    */
+    void launchBrowser()
+    {
+        cout << "Запускаем браузер" << endl;
+    }
 };
 
+/// \brief Абстрактный класс команды
 class Command
 {
 public:
+    /**
+    * brief Выполнение команды
+    */
     virtual void execute() = 0;
+
+    /**
+    * brief Отмена команды
+    */
     virtual void undo() = 0;
 };
 
+/// \brief Команда открытия файла
 class OpenFileCommand : public Command
 {
 private:
-    ComputerSystem* system;
-    string fileName;
+    ComputerSystem* system;   ///< Указатель на систему
+    string fileName;          ///< Имя файла
+
 public:
-    OpenFileCommand(ComputerSystem* s, const string& fn) : system(s), fileName(fn) {}
-    void execute() override { system->openFile(fileName); }
-    void undo() override { cout << "Закрываем файл: " << fileName << endl; }
+    /**
+    * brief Конструктор команды открытия файла
+    * note Сохраняет ссылку на систему и имя файла
+    */  
+    OpenFileCommand(ComputerSystem* s, const string& fn)
+        : system(s), fileName(fn) {
+    }
+
+    /**
+    * brief Выполнение команды
+    */
+    void execute() override
+    {
+        system->openFile(fileName);
+    }
+
+    /**
+    * brief Отмена команды
+    */
+    void undo() override
+    {
+        cout << "Закрываем файл: " << fileName << endl;
+    }
 };
 
+/// \brief Команда выключения компьютера
 class ShutdownCommand : public Command
 {
 private:
-    ComputerSystem* system;
+    ComputerSystem* system;   ///< Указатель на систему
+
 public:
-    ShutdownCommand(ComputerSystem* s) : system(s) {}
-    void execute() override { system->shutdown(); }
-    void undo() override { cout << "Отмена: Включаем компьютер обратно" << endl; }
+    /**
+    * brief Конструктор команды выключения
+    */
+    ShutdownCommand(ComputerSystem* s)
+        : system(s) {
+    }
+
+    /**
+    * brief Выполнение команды
+    */
+    void execute() override
+    {
+        system->shutdown();
+    }
+
+    /**
+    * brief Отмена команды
+    */
+    void undo() override
+    {
+        cout << "Отмена: Включаем компьютер обратно" << endl;
+    }
 };
 
+/// \brief Команда запуска браузера
 class LaunchBrowserCommand : public Command
 {
 private:
-    ComputerSystem* system;
+    ComputerSystem* system;   ///< Указатель на систему
+
 public:
-    LaunchBrowserCommand(ComputerSystem* s) : system(s) {}
-    void execute() override { system->launchBrowser(); }
-    void undo() override { cout << "Закрываем браузер" << endl; }
+    /**
+    * brief Конструктор команды запуска браузера
+    * note Сохраняет ссылку на систему
+    */
+    LaunchBrowserCommand(ComputerSystem* s)
+        : system(s) {
+    }
+
+    /**
+    * brief Выполнение команды
+    */
+    void execute() override
+    {
+        system->launchBrowser();
+    }
+
+    /**
+    * brief Отмена команды
+    */
+    void undo() override
+    {
+        cout << "Закрываем браузер" << endl;
+    }
 };
 
+/// \brief Класс пульта управления
 class RemoteControl
 {
 private:
     vector<Command*> commands;
+
 public:
-    void addCommand(Command* cmd) { commands.push_back(cmd); }
+    /**
+    * brief Добавление команды в список
+    */
+    void addCommand(Command* cmd)
+    {
+        commands.push_back(cmd);
+    }
+
+    /**
+    * brief Нажатие кнопки 
+    */
     void pressButton()
     {
         for (auto cmd : commands)
@@ -62,6 +170,11 @@ public:
             cmd->execute();
         }
     }
+
+    /**
+    * brief Отмена последней добавленной команды
+    * note Вызывает undo() и удаляет команду из списка
+    */
     void undoLast()
     {
         if (!commands.empty())
@@ -71,27 +184,39 @@ public:
         }
     }
 };
+
 int main()
 {
     setlocale(LC_ALL, "ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    /// Создание системы (получателя)
     ComputerSystem system;
 
+    /// Создание конкретных команд
     Command* openCmd = new OpenFileCommand(&system, "document.txt");
     Command* shutdownCmd = new ShutdownCommand(&system);
     Command* browserCmd = new LaunchBrowserCommand(&system);
 
+    /// Создание пульта (инициатора)
     RemoteControl remote;
+
+    /// Добавление команд в пульт
     remote.addCommand(openCmd);
     remote.addCommand(browserCmd);
     remote.addCommand(shutdownCmd);
 
+    /// Выполнение всех команд
     remote.pressButton();
+
+    /// Отмена последней команды
     remote.undoLast();
 
+    /// Освобождение памяти
     delete openCmd;
     delete shutdownCmd;
     delete browserCmd;
+
+    return 0;
 }
